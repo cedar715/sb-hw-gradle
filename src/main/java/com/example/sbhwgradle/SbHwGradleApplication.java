0ab6780cc4d0a691831f73dcb5a2aee7a756ddd5
prakashj <Jaya.Prakash@signetjewelers.com>
2021-12-21 21:18:10 +0530

    Initial commit

---
+++ src/main/java/com/example/sbhwgradle/SbHwGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbhwgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHwGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbHwGradleApplication.class, args);
+    }
+
+}
